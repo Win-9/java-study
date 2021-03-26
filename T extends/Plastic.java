78e2e1759cdf7eb419369a76adcee0f7cd12f68f
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-03-26 15:48:12 +0900

    Add files via upload

---
+++ T extends/Plastic.java
@@ -0,0 +1,13 @@
+package ex2;
+
+public class Plastic extends Material{
+	public String toString() {
+		return "material-power";
+	}
+
+	@Override
+	public void doPrinting() {
+		// TODO Auto-generated method stub
+		
+	}
+}
