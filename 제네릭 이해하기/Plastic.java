c9fad26ac58133b3bc40dea4736e837c62dad29c
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-06-08 23:12:32 +0900

    Delete 제네릭 이해하기 directory

--- 제네릭 이해하기/Plastic.java
+++
@@ -1,7 +0,0 @@
-package ex1;
-
-public class Plastic {
-	public String toString() {
-		return "material-power";
-	}
-}
--- 제네릭 이해하기/Powder.java
+++
@@ -1,7 +0,0 @@
-package ex1;
-
-public class Powder {
-	public String toString() {
-		return "material-power";
-	}
-}
