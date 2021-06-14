33693d1944e10f301dea7e910f646dcc35354871
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-06-14 22:59:03 +0900

    Create FileReaderTest.java

---
+++ 입출력/입출력연습/FileReaderTest.java
@@ -0,0 +1,14 @@
+public class FileReaderTest {
+
+	public static void main(String[] args) {
+
+		try(FileReader fr = new FileReader("reader.txt")){
+			int i;
+			while( (i = fr.read()) != -1){
+				System.out.print((char)i);
+			}
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
