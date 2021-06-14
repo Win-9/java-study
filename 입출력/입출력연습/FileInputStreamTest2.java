15475bc72b795d5092d94ffbf56f44c33e36c70f
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-06-14 21:51:54 +0900

    Create FileInputStreamTest2.java

---
+++ 입출력/입출력연습/FileInputStreamTest2.java
@@ -0,0 +1,18 @@
+public class FileInputStreamTest2 {
+
+	public static void main(String[] args) {
+
+		try(FileInputStream fis = new FileInputStream("input.txt")){ 
+			int i;
+			while ( (i = fis.read()) != -1){
+				System.out.println((char)i);
+			}
+			System.out.println("end");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
