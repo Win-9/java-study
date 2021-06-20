8e815dc3819b4484d49264a940729a916f9ec8e1
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-06-20 14:57:41 +0900

    Create AClazz.java

---
+++ SpringWeb/디자인 패턴/Singleton/AClazz.java
@@ -0,0 +1,16 @@
+public class AClazz {
+
+    public SocketClient getSocketClient() {
+        return socketClient;
+    }
+
+    public void setSocketClient(SocketClient socketClient) {
+        this.socketClient = socketClient;
+    }
+
+    private SocketClient socketClient;
+
+    public AClazz(){
+        this.socketClient = SocketClient.getInstance();
+    }
+}
