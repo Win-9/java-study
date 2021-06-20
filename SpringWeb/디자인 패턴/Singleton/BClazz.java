1049aa9010709493a9d229a437a21d70321913cf
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-06-20 14:57:57 +0900

    Create BClazz.java

---
+++ SpringWeb/디자인 패턴/Singleton/BClazz.java
@@ -0,0 +1,16 @@
+public class BClazz {
+
+    public SocketClient getSocketClient() {
+        return socketClient;
+    }
+
+    public void setSocketClient(SocketClient socketClient) {
+        this.socketClient = socketClient;
+    }
+
+    private SocketClient socketClient;
+
+    public BClazz(){
+        this.socketClient = SocketClient.getInstance();
+    }
+}
