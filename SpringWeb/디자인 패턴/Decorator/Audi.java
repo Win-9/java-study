9239dc096b8f017879af3c0ab0fc13f9a010c535
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-06-21 22:49:18 +0900

    Create Audi.java

---
+++ SpringWeb/디자인 패턴/Decorator/Audi.java
@@ -0,0 +1,18 @@
+public class Audi implements ICar{
+
+    private int cost;
+
+    public Audi(int cost){
+        this.cost = cost;
+    }
+
+    @Override
+    public int getPrice(){
+        return this.cost;
+    }
+
+    @Override
+    public void showCost() {
+        System.out.println("Audi Base는 "+cost+" 원 입니다.");
+    }
+}
