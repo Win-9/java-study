a90db956cb48aa989ed10c5880f62882cce0b070
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-06-21 22:53:41 +0900

    Create Encoder.java

---
+++ SpringWeb/디자인 패턴/Strategy/Encoder.java
@@ -0,0 +1,11 @@
+public class Encoder {
+    private EncodingStrategy encodingStrategy;
+
+    public String getMessage(String message){
+        return encodingStrategy.encoding(message);
+    }
+
+    public void setEncodingStrategy(EncodingStrategy encodingStrategy){
+        this.encodingStrategy = encodingStrategy;
+    }
+}
