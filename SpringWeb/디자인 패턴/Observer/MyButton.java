e95777887d1c18a682454fac5beab1aaf6477da6
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-06-21 22:50:32 +0900

    Create MyButton.java

---
+++ SpringWeb/디자인 패턴/Observer/MyButton.java
@@ -0,0 +1,16 @@
+public class MyButton {
+    private String name;
+    private IButtonClickListener buttonClickListener;
+
+    public MyButton(String buttonName){
+        this.name = buttonName;
+    }
+
+    public void click(String clickEvent){
+        buttonClickListener.clickEvent(this.name+", "+clickEvent);
+    }
+
+    public void addListener(IButtonClickListener buttonClickListener){
+        this.buttonClickListener = buttonClickListener;
+    }
+}
