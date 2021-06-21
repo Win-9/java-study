099fdd6db8c4e09cbf780baa1e344d37a5c175ff
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-06-21 22:52:05 +0900

    Create FileWriter.java

---
+++ SpringWeb/디자인 패턴/Facade/FileWriter.java
@@ -0,0 +1,18 @@
+public class FileWriter {
+
+    public FileWriter(String fileName){
+
+    }
+
+    public void fileConnect(){
+        System.out.println("FileWriter Connected");
+    }
+
+    public void fileWrite(String content){
+        System.out.println("write : "+content);
+    }
+
+    public void fileDisconnect(){
+        System.out.println("FileWriter disConnected");
+    }
+}
