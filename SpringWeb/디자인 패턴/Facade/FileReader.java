650c736eec2c2c9c492754cd4f9870da6a50012b
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-06-21 22:52:24 +0900

    Create FileReader.java

---
+++ SpringWeb/디자인 패턴/Facade/FileReader.java
@@ -0,0 +1,18 @@
+public class FileReader {
+
+    public FileReader(String fileName){
+
+    }
+
+    public void fileConnect(){
+        System.out.println("FileReader Connected");
+    }
+
+    public String fileRead(){
+        return "content";
+    }
+
+    public void fileDisconnect(){
+        System.out.println("FileReader disConnected");
+    }
+}
