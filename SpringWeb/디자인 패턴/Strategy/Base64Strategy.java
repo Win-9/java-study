f972c3adc3b2327ef436ac89a229c4dffd72e91f
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-06-21 22:54:01 +0900

    Create Base64Strategy.java

---
+++ SpringWeb/디자인 패턴/Strategy/Base64Strategy.java
@@ -0,0 +1,8 @@
+import java.util.Base64;
+
+public class Base64Strategy implements EncodingStrategy{
+    @Override
+    public String encoding(String message) {
+        return Base64.getEncoder().encodeToString(message.getBytes());
+    }
+}
