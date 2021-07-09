8ce6999afe4b57dca87443289991fbc30c2673ef
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-07-09 21:07:30 +0900

    Create MvcConfig.java

---
+++ SpringWeb/Filter & Interceptor/Interceptor/config/MvcConfig.java
@@ -0,0 +1,20 @@
+package config;
+
+
+import com.example.interceptor.interceptor.AuthInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor//fianl로 생성된 객체들을 생성자에서 주입받을수 있도록 함.
+public class MvcConfig implements WebMvcConfigurer {
+
+    private final AuthInterceptor authInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor).addPathPatterns("/api/private/*");
+    }
+}
