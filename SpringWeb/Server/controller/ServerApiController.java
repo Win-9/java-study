c2c7f318b18cc67891436b62ed2ce00ebe09aa67
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-07-20 18:04:23 +0900

    Update ServerApiController.java

--- SpringWeb/Server/controller/ServerApiController.java
+++ SpringWeb/Server/controller/ServerApiController.java
@@ -21,4 +21,65 @@ public class ServerApiController {
 
         return user;
     }
+    public UserResponse exchange(){
+        URI uri=UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("/api/server/user/{userId}/name/{userName}")
+                .encode()
+                .build()
+                .expand("100","steve")
+                .toUri();
+
+        System.out.println(uri);
+
+        UserRequest req=new UserRequest();
+        req.setName("steve");
+        req.setAge(10);
+
+        RequestEntity<UserRequest>requestEntity=RequestEntity
+                .post(uri)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("x-authorization","abcd")
+                .header("custom-header","fff")
+                .body(req);
+
+        RestTemplate restTemplate=new RestTemplate();
+        ResponseEntity<UserResponse>response=restTemplate.exchange(requestEntity, UserResponse.class);
+
+        return response.getBody();
+
+    }
+
+    public UserResponse genericExchange(){
+        URI uri=UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("/api/server/user/{userId}/name/{userName}")
+                .encode()
+                .build()
+                .expand("100","steve")
+                .toUri();
+        System.out.println(uri);
+        UserRequest userRequest=new UserRequest();
+        userRequest.setName("steve");
+        userRequest.setAge(10);
+
+        Req<UserRequest> req=new Req<UserRequest>();
+        req.setHeader(new Req.Header());
+        req.setBody(userRequest);
+
+        RequestEntity<Req<UserRequest>>requestEntity=RequestEntity
+                .post(uri)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("x-authorization","abcd")
+                .header("custom-header","fff")
+                .body(req);
+
+        RestTemplate restTemplate=new RestTemplate();
+
+
+        ResponseEntity<Req<UserResponse>> response = restTemplate.exchange(
+                requestEntity, new ParameterizedTypeReference<Req<UserResponse>>() {});
+
+        return response.getBody().getBody();
+    }
 }
