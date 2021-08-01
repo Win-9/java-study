e96f409c2fc55c6c4bc1e0b5265d9c38d143bca2
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-08-01 17:51:30 +0900

    Create MyFrame.java

---
+++ Swing/실습예제/ex1/MyFrame.java
@@ -0,0 +1,29 @@
+package Swing.ex1;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class MyFrame extends JFrame{
+	
+	public MyFrame() {
+		setTitle("Let's study Java");
+		setSize(400,200);
+		setVisible(true);
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyFrame f=new MyFrame();
+		
+	}
+
+}
