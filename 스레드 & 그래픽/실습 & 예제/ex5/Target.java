fb60b3afc5cb7671a5874a61bd73e9ea46b1842f
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-09-08 15:20:57 +0900

    Update Target.java

--- 스레드 & 그래픽/실습 & 예제/ex5/Target.java
+++ 스레드 & 그래픽/실습 & 예제/ex5/Target.java
@@ -9,56 +9,51 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class Target extends Thread{
+public class Target extends Thread {
 	private Container container;
 	private JLabel targetLable;
 	private int targetLocationX;
-	private boolean reverseFlag=false;
-	private boolean endFlag=false;
-	public Target(JLabel targetLable,Container container) {
-		this.targetLable=targetLable;
-		this.container=container;
-		int targetLocationX=targetLable.getX();
+	private boolean reverseFlag = false;
+	private boolean endFlag = false;
+
+	public Target(JLabel targetLable, Container container) {
+		this.targetLable = targetLable;
+		this.container = container;
+		int targetLocationX = targetLable.getX();
 	}
 
-	
 	public void run() {
-		while(true) {
-			
-			if(reverseFlag) {
-				targetLocationX+=5;
+		while (true) {
+			if (reverseFlag) {
+				targetLocationX -= 5;
+				targetLable.setLocation(targetLocationX, 0);
+			} else {
+				targetLocationX += 5;
 				targetLable.setLocation(targetLocationX, 0);
 			}
-			else {
-				targetLocationX-=5;
-				targetLable.setLocation(targetLocationX, 0);
-			}
-			
+
+			setReverseFlag();
+
 			try {
 				Thread.sleep(20);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
-			if(endFlag) {
-				targetLable.setLocation(40,0);
+
+			if (endFlag) {
+				targetLable.setLocation(40, 0);
 			}
-			
+
 		}
 	}
-	
+
 	public void setReverseFlag() {
-		if(targetLocationX==container.getX()) {
-			reverseFlag=true;
+		if (targetLocationX + 50 >= container.getWidth()) {
+			reverseFlag = true;
+		} else if (targetLocationX == 0) {
+			reverseFlag = false;
 		}
-		else if(targetLocationX==0){
-			reverseFlag=false;
-		}
-	}
-	
-	public void finish() {
-		endFlag=true;
 	}
 
 }
