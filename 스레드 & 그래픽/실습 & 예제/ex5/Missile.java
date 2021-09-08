c6afa81e4b3ad44f99196ad5429f8a3ea170865f
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-09-08 15:20:24 +0900

    Update Missile.java

--- 스레드 & 그래픽/실습 & 예제/ex5/Missile.java
+++ 스레드 & 그래픽/실습 & 예제/ex5/Missile.java
@@ -8,36 +8,43 @@ import java.awt.event.KeyEvent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class Missile extends KeyAdapter implements Runnable{
+public class Missile extends Thread {
 	private JLabel missileLaunch;
 	private JLabel player;
-	
-	public Missile(JLabel missileLaunch,JLabel player) {
+	private boolean endFlag = false;
+
+	public Missile(JLabel missileLaunch, JLabel player) {
 		// TODO Auto-generated constructor stub
-		this.missileLaunch=this.missileLaunch;
-		this.player=player;
+		this.missileLaunch = missileLaunch;
+		this.player = player;
 	}
-	
-	
+
 	public void run() {
-		
-		int missileY=player.getY();
-		int missileX=player.getX();
-		
-		while(missileY!=0) {
+		missileLaunch.setOpaque(true);
+		int missileY = player.getY();
+		int missileX = player.getX();
+		while (missileY >= 0) {
 			missileLaunch.setLocation(missileX, missileY);
-			missileY-=10;
-			
+			missileY -= 10;
+
 			try {
-				Thread.sleep(200);
+				Thread.sleep(20);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
-			
+
+			if (endFlag) {
+				return;
+			}
+
 		}
-		
+		missileLaunch.setOpaque(false);
+		System.out.println("end");
+	}
+
+	public void finish() {
+		endFlag = true;
 	}
 
 }
