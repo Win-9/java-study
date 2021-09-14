3595cdb9b6303000a66692d9d0f4439e089ecb83
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-09-14 19:00:43 +0900

    Update GameLabel.java

--- 스레드 & 그래픽/실습 & 예제/ex9/GameLabel.java
+++ 스레드 & 그래픽/실습 & 예제/ex9/GameLabel.java
@@ -4,7 +4,8 @@ import java.awt.Color;
 import java.awt.Container;
 import java.awt.Font;
 import java.awt.event.MouseAdapter;
-
+import java.awt.event.MouseListener;
+import java.awt.event.*;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -13,27 +14,36 @@ import org.w3c.dom.events.MouseEvent;
 public class GameLabel extends JPanel {
 	private JLabel[] gameLabel;
 	private GamblingThread gt;
-
-	public GameLabel(JLabel[] gameLabel, GamblingThread gt) {
+	private JLabel last;
+	public GameLabel(JLabel[] gameLabel, GamblingThread gt,JLabel last) {
 		// TODO Auto-generated constructor stub
 		this.gameLabel = gameLabel;
 		this.gt = gt;
+		this.last=last;
 		for (int i = 0; i < 3; i++) {
 			JPanel panel = new JPanel();
 			gameLabel[i].setFont(new Font("Arial", Font.ITALIC, 50));
 			gameLabel[i].setForeground(Color.YELLOW);
 			panel.add(gameLabel[i]);
 			panel.setSize(100, 50);
-			panel.setLocation(100 * (i + 30), 50);
+			panel.setLocation(100 * (i + 30), 100);
 			panel.setBackground(Color.MAGENTA);
 			add(panel);
-			panel.addMouseListener(new MouseAdapter() {
-				public void mouseClicked(MouseEvent e) {
-					System.out.println("시이발");
+			addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseClicked(java.awt.event.MouseEvent e) {
+					// TODO Auto-generated method stub
 					gt.startGambling();
 				}
 			});
+			
+			panel.requestFocus();
 		}
+		last.setFont(new Font("Gothic",Font.BOLD,20));
+		last.setBackground(Color.LIGHT_GRAY);
+		last.setOpaque(true);
+		last.setLocation(400, 400);
+		add(last);
 
 	}
 
