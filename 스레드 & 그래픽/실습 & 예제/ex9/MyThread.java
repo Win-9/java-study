4057b5b4cbd56cbd16d0888236f76bc35204c48b
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-09-14 19:00:56 +0900

    Update MyThread.java

--- 스레드 & 그래픽/실습 & 예제/ex9/MyThread.java
+++ 스레드 & 그래픽/실습 & 예제/ex9/MyThread.java
@@ -8,7 +8,7 @@ import javax.swing.JLabel;
 
 public class MyThread extends JFrame {
 	private JLabel[] gameLabel = new JLabel[3];
-
+	private JLabel last=new JLabel("클릭하면 시작");
 	void run() {
 		setTitle("ex");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -16,13 +16,16 @@ public class MyThread extends JFrame {
 		for (int i = 0; i < 3; i++) {
 			gameLabel[i] = new JLabel("0");
 		}
-		GamblingThread gt = new GamblingThread(gameLabel);
-
-		setContentPane(new GameLabel(gameLabel, gt));
+		GamblingThread gt = new GamblingThread(gameLabel,last);
+		GameLabel game=new GameLabel(gameLabel, gt,last);
+		setContentPane(game);
 
 
 		setSize(300, 300);
 		setVisible(true);
+		
+		getContentPane().setFocusable(true);
+		getContentPane().requestFocus();
 	}
 
 	public static void main(String[] args) {
